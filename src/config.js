2c103a07e67c6064322e3dda558c7f989af10ae0
anandvyas1290 <anandvyas1290@gmail.com>
2022-08-14 04:59:33 +0530

    project logo and name change,adding more features to header

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import offer from "assets/offer.png";
+import help from "assets/help.png";
+import user from "assets/user.png";
+
+export const navList = [
+  { _id: "offers", label: "Offers", icon: offer, link: "/offers" },
+  { _id: "help", label: "Help", icon: help, link: "/support" },
+  { _id: "my_account", label: "Account", icon: user, link: "/my-account" },
+];
