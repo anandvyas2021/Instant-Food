fed1ef3ab419451049fd00a2708ba9b5fb4881a5
anandvyas1290 <anandvyas1290@gmail.com>
2022-08-30 23:05:13 +0530

    regular commit

--- src/Components/carousel/index.js
+++ src/Components/carousel/index.js
@@ -1,55 +1,25 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import Slider from "react-slick";
+import { Link } from "react-router-dom";
 import { carouselList } from "config";
 
-function NextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block" }}
-            onClick={onClick}
-        />
-    );
-}
-
-function PrevArrow(props) {
-    console.log(props);
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block" }}
-            onClick={onClick}
-        />
-    );
-}
 export default function Carousel() {
-    var settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: <PrevArrow />,
-        nextArrow: <NextArrow />,
-    };
-    return (
-        <Slider {...settings}>
-            {carouselList.map((item) => (
-                <div className={styles["carousel-images-box"]} key={item._id}>
-                    <img
-                        className={styles["carousel-image"]}
-                        src={item.image}
-                        alt="slider"
-                    />
-                </div>
-            ))}
-        </Slider>
-    );
+  return (
+    <div className={styles["carousel-wrapper"]}>
+      <div className={styles["carousel"]}>
+        {carouselList.map((item) => (
+          <Link
+            to={item.slug}
+            key={item._id}
+            className={styles["carousel-image-box"]}
+          >
+            <img src={item.image} className={styles["carousel-image"]} />
+          </Link>
+        ))}
+        <div className={styles["carousel-prev-btn carousel-btn"]}>&#8249;</div>
+        <div className={styles["carousel-next-btn carousel-btn"]}>&#8250;</div>
+      </div>
+    </div>
+  );
 }
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,20 +2,24 @@ import React from "react";
 import styles from "./styles.module.css";
 import MealsImg from "assets/meals.jpg";
 
-import MealsSummary from "components/Meals/MealsSummary";
+import MealsSummary from "components/meals/Meals";
+// import Carousel from "components/slick";
 import Carousel from "components/carousel";
+import Slick from "components/slick";
 
 export default function Home() {
-    return (
-        <React.Fragment>
-            <div className={styles["main-image-section"]}>
-                <img src={MealsImg} alt="Dishes on the table" />
-            </div>
+  return (
+    <React.Fragment>
+      <div className={styles["main-image-section"]}>
+        <img src={MealsImg} alt="Dishes on the table" />
+      </div>
 
-            <MealsSummary />
-            <div className={styles["home-carousel-container"]}>
-                <Carousel />
-            </div>
-        </React.Fragment>
-    );
+      <MealsSummary />
+      <div className={styles["home-carousel-container"]}>
+        {/* <Carousel /> */}
+        <Slick />
+        <div className={styles["home-carousel"]}></div>
+      </div>
+    </React.Fragment>
+  );
 }
