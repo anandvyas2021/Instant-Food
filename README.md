00ccfcd4c99ad5f23ec52c50e0a13f0c009b5a45
anandvyas1290 <anandvyas1290@gmail.com>
2022-09-02 02:05:33 +0530

    some minor designing in restro card

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ Instant Food - A online food ordering app
 
 Technologies used:-
 Frontend :- React.js, SCSS, React-Redux, etc...
-Backend :- Firebase(for now, Node.js(in future)
+Backend :- Firebase(for now), Node.js(in future)
+Others :- Antd
 
 This project provides all the facilties that a modern online food delivery app provides.
 
-Features:-
+<!-- Features:- -->
--- src/custom/RestaurantCard/index.js
+++ src/custom/RestaurantCard/index.js
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
+import star from "assets/star.png";
 import offer from "assets/NavIcons/offer.png";
 
 import { Tag } from "antd";
 
 export default function RestaurantCard(props) {
+  const [state, setState] = useState({ mouseHover: false });
+
+  // useEffect(() => {
+  //   let card = document.querySelector(".restro-card-box");
+  //   console.log(card);
+  //   console.log(state.mouseHover);
+  //   // document.addEventListener('mouseover')
+
+  //   return () => {};
+  // }, [state.mouseHover]);
+
+  const mouseOverCard = () => {
+    setState({ mouseHover: true });
+    let card = document.querySelector(".restro-card-box");
+    console.log(card);
+  };
+
+  const mouseLeaveCard = () => {};
+
   const { data } = props;
-  console.log(data);
   return (
     <div className={styles["restro-card-box"]}>
       {/* <span className={styles["restro-card-ribbon"]} style={data.ribbon_styles}>
         {data.ribbon}
       </span> */}
-      <div className={styles["restro-card"]}>
+      <div
+        className={styles["restro-card"]}
+        onMouseOver={() => mouseOverCard()}
+        onMouseLeave={() => mouseLeaveCard()}
+      >
         <div className={styles["restro-card-image"]}>
           <img src={data.image} alt="restro" width="100%" height="100%" />
         </div>
         <div className={styles["restro-card-description"]}>
-          <h4>{data.name}</h4>
-          <div>
-            {data.tags.map((tag) => (
-              <Tag color="yellow">{tag}</Tag>
-            ))}
+          <h4
+            style={{
+              fontSize: "17px",
+              fontWeight: 500,
+              wordBreak: "break-word",
+            }}
+          >
+            {data.name}
+          </h4>
+          <div className={styles["restro-card-tags"]}>
+            {data.tags.map((tag) => {
+              return <Tag color="red">{tag}</Tag>;
+            })}
           </div>
         </div>
         <div className={styles["restro-card-details"]}>
@@ -31,7 +62,9 @@ export default function RestaurantCard(props) {
               backgroundColor: Number(data.rating) > 4 ? "#48c479" : "#db7c38",
             }}
           >
-            <span className={styles["details-rating-star"]} />
+            <span className={styles["details-rating-star"]}>
+              <img src={star} width="100%" height="100%" />
+            </span>
             <span>{data.rating}</span>
           </div>
           <div>{"•"}</div>
@@ -51,6 +84,12 @@ export default function RestaurantCard(props) {
             }`}</span>
           </div>
         )}
+
+        <div className={styles["restro-card-footer"]}>
+          <span className={styles["restro-card-footer-text"]}>
+            {"QUICK VIEW"}
+          </span>
+        </div>
       </div>
     </div>
   );
