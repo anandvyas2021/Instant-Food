d8b47f91cf29fc8a870647152c043ffc2e99daec
anand-vyas-gg <anand.vyas@globalgarner.com>
2022-09-20 19:30:09 +0530

    working on offers route

--- src/routes/Offers/index.js
+++ src/routes/Offers/index.js
@@ -1,6 +1,41 @@
 import React from "react";
-import "./styles.module.scss";
+import styles from "./styles.module.scss";
+import offerBanner from "assets/offers.webp";
+
+import Tabs from "components/BasicComponents/Tabs";
+import { offersTabpanes } from "utils/config";
 
 export default function Offers() {
-    return <div>Offers</div>;
+    const onClick = (e) => {
+        console.log(e);
+    };
+
+    return (
+        <div className={styles["offers-page"]}>
+            <div className={styles["offers-banner-container"]}>
+                <div className={styles["offers-banner"]}>
+                    <div className={styles["offers-banner-inner"]}>
+                        <div className={styles["offers-banner-label"]}>
+                            <div className={styles["offers-banner-heading"]}>
+                                Offers for you
+                            </div>
+                            <div className={styles["offers-banner-desc"]}>
+                                Explore top deals and offers exclusively for
+                                you!
+                            </div>
+                        </div>
+                        <div className={styles["offers-banner-image"]}>
+                            <img src={offerBanner} alt="banner" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles["offers-tabs-container"]}>
+                <Tabs options={offersTabpanes} onClick={onClick} />
+            </div>
+
+            <div className={styles["offers-body-container"]}></div>
+        </div>
+    );
 }
